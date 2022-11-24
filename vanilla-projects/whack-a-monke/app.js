9333bef83f0da316252f31dd404dfa85edb1dae2
SBagaria2710 <shashwawtforwork@gmail.com>
2022-11-25 03:23:22 +0530

    Finish whack a monke

--- vanilla-projects/whack-a-monke/app.js
+++ vanilla-projects/whack-a-monke/app.js
@@ -1,48 +1,83 @@
 const scoreElem = document.getElementById("score");
 const timerElem = document.getElementById("timer");
-const initCta = document.getElementById("initCta");
-const gridElem = document.querySelector(".grid");
+const resetBtn = document.getElementById("reset-game");
+const startBtn = document.getElementById("start-game");
+const gridContainer = document.querySelector(".grid-container");
+// const gameOverPopUp = document.querySelector(".grid-container");
 
-const MAX_SIZE = 5;
 const n = 3;
 let timerId = null;
 let score = 0;
-let count = 0;
+let timeRemaining = 5;
+let isClicked = false;
 
-function init(n) {
-    if (n > MAX_SIZE) n = 5;
-    for (let i = 1; i <= n*n; i++) {
+function init() {
+    for (let i = 0; i < n*n; i++) {
         const cell = document.createElement("div");
         cell.classList.add("grid-cell");
         cell.setAttribute(`data-position`, i);
-        gridElem.style.gridTemplateColumns = `repeat(${n}, auto)`
-        gridElem.append(cell);
+        gridContainer.style.gridTemplateColumns = `repeat(${n}, auto)`
+        gridContainer.append(cell);
     }
+    resetBtn.disabled = true;
+    timerElem.querySelector("span").textContent = timeRemaining;
 }
 
-gridElem.addEventListener("click", (e) => {
+gridContainer.addEventListener("click", (e) => {
+    if (isClicked) return;
+
     const cell = e.target;
-    console.log(cell.dataset.position);
     if (cell.classList.contains("monke")) {
         score += 1;
-        scoreElem.textContent = score;
+        isClicked = true;
+        scoreElem.querySelector("span").textContent = score;
+        cell.classList.remove("monke")
     }
 });
 
 function startGame() {
-    let elem;
+    resetBtn.disabled = false;
+    startBtn.disabled = true;
+
     timerId = setInterval(function() {
-        if (!!elem) {
-            elem.classList.remove("monke");
-        }
-        if (count === 5 && timerId) {
+        // Show and hide Monke on a random cell
+        const randomCell = Math.floor(Math.random()*n*n);
+        randomGridElem = document.querySelector(`[data-position='${randomCell}']`);
+        randomGridElem.classList.add("monke");
+        setTimeout(function() {
+            randomGridElem.classList.remove("monke");
+            isClicked = false;
+        }, 800);
+
+        // Update Timer
+        timeRemaining -= 1;
+        timerElem.querySelector("span").textContent = timeRemaining;
+
+        // Check if time is up
+        if (timeRemaining === -1) {
             clearInterval(timerId);
+            gameOver();
         }
+    }, 1000);
+};
 
-        elem = document.querySelector(`[data-position='${Math.floor(Math.random()*n*n)}']`);
-        elem.classList.add("monke");
-        count = count + 1;
-    }, 2000);
-}
+function resetGame() {
+    timerId = null;
+    score = 0;
+    timeRemaining = 5;
+    isClicked = false;
+    startBtn.disabled = false;
+    timerElem.querySelector("span").textContent = timeRemaining;
+    scoreElem.querySelector("span").textContent = score;
+};
 
-init(n);
+function gameOver() {
+    resetBtn.disabled = true;
+    startBtn.disabled = true;
+    alert(`Game Over! Your final score is ${score} *slow claps*`);
+
+    // Reset Everything Back
+    resetGame()
+};
+
+init();
